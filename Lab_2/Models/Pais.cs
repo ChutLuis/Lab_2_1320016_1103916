bef18ec938a4d90ef6a25b3fab405eb7037358e3
ChutLuis <l.chuta2014ig@gmail.com>
2018-02-26 19:25:22 -0600

    Primer Commit

---
+++ Lab_2/Models/Pais.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace Lab_2.Models
+{
+    public class Pais
+    {
+        private string NombrePais;
+        private string Grupo;
+
+        public string NombrePais1
+        {
+            get
+            {
+                return NombrePais;
+            }
+
+            set
+            {
+                NombrePais = value;
+            }
+        }
+
+        public string Grupo1
+        {
+            get
+            {
+                return Grupo;
+            }
+
+            set
+            {
+                Grupo = value;
+            }
+        }
+    }
+}
