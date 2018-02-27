7de24a3332d230599dd61c72a5d67d6326610e0f
ChutLuis <l.chuta2014ig@gmail.com>
2018-02-26 21:34:53 -0600

    xd

---
+++ ClassLibrary/ArbolBinario.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace ClassLibrary
+{
+    class ArbolBinario<T>:Node<T>, IComparable<T>
+    {
+        
+        private Node<T> Raiz;
+        public ArbolBinario()
+        {
+            Raiz = null;
+            
+        }       
+
+        public void Insert(Node<T> nodo, T value)
+        {
+            if (Raiz == null)
+                Raiz = nodo;
+            if(CompareTo())
+
+
+        }
+
+        public int CompareTo(T other)
+        {
+            
+        }
+
+    }
+}
---
+++ ClassLibrary/Node.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ClassLibrary
+{
+    public class Node<T>:
+    {
+        T value;
+        Node<T> left;
+        Node<T> right;
+
+        public T Value
+        {
+            get
+            {
+                return value;
+            }
+
+            set
+            {
+                this.value = value;
+            }
+        }
+
+        public Node<T> Left
+        {
+            get
+            {
+                return left;
+            }
+
+            set
+            {
+                left = value;
+            }
+        }
+
+        public Node<T> Right
+        {
+            get
+            {
+                return right;
+            }
+
+            set
+            {
+                right = value;
+            }
+        }
+
+        
+    }
+}
